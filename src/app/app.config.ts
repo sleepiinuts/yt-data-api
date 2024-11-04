e026a4d24555d01de6e2dbe6a099474d45b32f92
sleepynut <nut.pa@kbtg.tech>
2024-11-04 11:42:06 +0700

    project set up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,18 +7,21 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { APP_CONFIG } from '../environments/app-config.token';
 import { environment } from '../environments/environment';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
+    provideRouter(routes, withInMemoryScrolling({
         scrollPositionRestoration: 'enabled',
-      })
-    ),
+    })),
     provideClientHydration(),
     provideHttpClient(withFetch()),
     { provide: APP_CONFIG, useValue: environment }, provideAnimationsAsync(),
-  ],
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+],
 };
