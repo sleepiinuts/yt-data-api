8798c3d256e67154cda3f4d95c3d81d3e5ab4103
sleepynut <nut.pa@kbtg.tech>
2024-11-06 16:45:55 +0700

    clean up app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { catchError, of } from 'rxjs';
-import { APP_CONFIG } from '../environments/app-config.token';
+
 import { YoutubeSearchComponent } from './components/youtube-search/youtube-search.component';
 
 @Component({
@@ -13,17 +12,7 @@ import { YoutubeSearchComponent } from './components/youtube-search/youtube-sear
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  private readonly appConfig = inject(APP_CONFIG);
-
   title = 'yt-data-api';
-  URI = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=marvel+snap+meta+deck&key=${this.appConfig.apiKey}`;
 
-  constructor(private httpClient: HttpClient) {
-    // this.httpClient.get(this.URI).subscribe(
-    //   (data) => console.log(`[SUCCESS]data: ${JSON.stringify(data)}`),
-    //   (err) => console.log(`[ERROR]get: ${err}`)
-    // );
-
-    console.log(this.appConfig.apiURL);
-  }
+  constructor(private httpClient: HttpClient) {}
 }
