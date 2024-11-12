4d9c1a97f4b4895bcc44f3ecf9474dd71940cd7c
sleepynut <nut.pa@kbtg.tech>
2024-11-12 11:38:35 +0700

    fix spec error

--- src/app/store/youtube/youtube.reducer.spec.ts
+++ src/app/store/youtube/youtube.reducer.spec.ts
@@ -1,11 +1,11 @@
-import { reducer, initialState } from './youtube.reducer';
+import { initialState, ytReducer } from './youtube.reducer';
 
 describe('Youtube Reducer', () => {
   describe('an unknown action', () => {
     it('should return the previous state', () => {
       const action = {} as any;
 
-      const result = reducer(initialState, action);
+      const result = ytReducer(initialState, action);
 
       expect(result).toBe(initialState);
     });
